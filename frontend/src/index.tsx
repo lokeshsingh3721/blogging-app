093a5ada3f5249be160ca8175064266a7d82c35d
lokeshsingh3721 <lokesh3721@gmail.com>
2024-03-01 18:29:10 +0530

    add navbar

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Tiptap from "./editor/editor";
+import Navbar from "./navbar";
 
 const Index = () => {
   return (
     <div>
+      <Navbar />
       <Tiptap />
     </div>
   );
